a2a3971e5148e05cb7c5171fb021c5366dafbeee
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-11-10 12:53:43 +0200

    Fetching Data With MySQLi

---
+++ website8/inc/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Blog</title>
+    <link rel="stylesheet" type="text/css"
+     href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/cerulean/bootstrap.min.css">
+</head>
+<body>
+<?php include('navbar.php'); ?>
